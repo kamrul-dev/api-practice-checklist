c9c22421118536cc29aabab9b7e3e13dbcf25899
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-24 11:58:13 +0600

    data is loaded on the UI

--- comments-api/js/comments.js
+++ comments-api/js/comments.js
@@ -1,13 +1,27 @@
 //load api from jsonplaceholder
 
-function loadData(){
+function loadData() {
     fetch('https://jsonplaceholder.typicode.com/comments')
-    .then(res => res.json())
-    .then(data => displayLoadData(data));
+        .then(res => res.json())
+        .then(data => displayLoadData(data));
 }
-loadData();
+// loadData();
 
 // display loaded data
-function displayLoadData(comments){
-    console.log(comments);
+function displayLoadData(comments) {
+
+    //get UI section for display data
+    const commentSection = document.getElementById('comment');
+    for (const comment of comments) {
+        console.log(comment);
+        const div = document.createElement('div');
+        div.classList.add("comment-div");
+        div.innerHTML = `
+            <h4>${comment.name}</h4>
+            <h5>${comment.email}</h5>
+            <p>${comment.body}</p>
+        `;
+        commentSection.appendChild(div);
+
+    }
 }
